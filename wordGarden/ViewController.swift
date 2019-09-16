8f0ed431017b580f0b6926b84433bb623516785d
Nick Haidari <haidari@bc.edu>
2019-09-16 01:10:16 -0400

    triggers updated 3.2

--- wordGarden/ViewController.swift
+++ wordGarden/ViewController.swift
@@ -10,11 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userGuessLabel: UILabel!
+    @IBOutlet weak var guessedLetterField: UITextField!
+    @IBOutlet weak var guessLetterButton: UIButton!
+    @IBOutlet weak var guessCountLabel: UILabel!
+    @IBOutlet weak var playAgainButton: UIButton!
+    @IBOutlet weak var flowerImageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("InviewDidLoad, is guessedLetterField the first responder?", guessedLetterField.isFirstResponder)
+    
     }
 
-
+    func updateUIAfterGuess(){
+        guessedLetterField.resignFirstResponder()
+        guessedLetterField.text = ""
+    }
+    
+    @IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
+        
+    }
+    
+    @IBAction func doneKeyPressed(_ sender: UITextField) {
+        
+        
+        updateUIAfterGuess()
+        
+        
+    }
+    
+    @IBAction func guessLetterButtonPressed(_ sender: Any) {
+        //we do this to tell app that we aren't still giving input
+        
+        
+        updateUIAfterGuess()
+        
+        
+    
+    }
+    
+    @IBAction func palyAgainButtonPressed(_ sender: Any) {
+    }
 }
 
